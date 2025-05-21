b66cb0b13502820a95aa694ae50846ac07969166
DarioArcanjo1 <darioarcanjo73@gmail.com>
2025-05-21 05:34:33 +0000

    adicionar o projecto

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "<p>hello world</p>"
+def home():
+    return render_template("home.html")
+
+@app.route("/web/inscricao", methods=["GET", "POST"])
+def inscricao_web():
+        data = request.form
+        return jsonify(data)
+     
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
